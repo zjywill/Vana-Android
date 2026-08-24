e9aa117f532814fa59335822ea7f86a5dd0c749b
zjywill <zjywill@hotmail.com>
2026-08-24 17:12:30 +0800

    chore: bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.pinapia.vana"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
